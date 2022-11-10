8cac096219536da2a10404f9b4efb692a061267e
lwinmoehein <dev.lwinmoehein@gmail.com>
2022-11-10 15:25:44 +0630

    fix test

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,7 +5,9 @@ namespace App\Providers;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\Facades\Gate;
-
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
+ 
 class AuthServiceProvider extends ServiceProvider
 {
     /**
