72ddd376040e5fbd2e2b56080b4567e49651bf48
lwinmoehein <dev.lwinmoehein@gmail.com>
2022-11-10 23:50:40 +0630

    add redirect for email verification

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard/{verified}', function ($verified) {
+Route::get('/dashboard', function ($verified) {
     return response()->redirect('/');
 });
