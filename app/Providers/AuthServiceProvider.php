8151bedd24b92510dfb67961100cdd389224f43e
lwinmoehein <dev.lwinmoehein@gmail.com>
2022-11-10 15:24:03 +0630

    test

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -29,5 +29,12 @@ class AuthServiceProvider extends ServiceProvider
         ResetPassword::createUrlUsing(function ($user, string $token) {
           return env('SPA_URL') . '/reset-password?token=' . $token;
         });
+
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage)
+                ->subject('Verify Email Address')
+                ->line('Click the button below to verify your email address.')
+                ->action('Verify Email Address', $url);
+        });
     }
 }
