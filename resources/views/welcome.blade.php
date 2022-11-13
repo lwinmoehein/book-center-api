2a39bf5ba1734a62e130b6a3c1f11b5ee3fe755e
lwinmoehein <dev.lwinmoehein@gmail.com>
2022-11-13 14:55:50 +0630

    ui improvements

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,27 +7,27 @@
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <link rel="icon" href="/favicon.ico">
     <title>laravel-vue</title>
-    <link href="/js/dashboard.b56292b3.js" rel="prefetch">
+    <link href="/js/dashboard.2943215e.js" rel="prefetch">
     <link href="/js/forgot-password.5560a71d.js" rel="prefetch">
-    <link href="/js/forgot-password~login~register~user.367af187.js" rel="prefetch">
+    <link href="/js/forgot-password~login~register~user.52cfcb11.js" rel="prefetch">
     <link href="/js/home.9c8ff869.js" rel="prefetch">
     <link href="/js/login.732526ec.js" rel="prefetch">
-    <link href="/js/not-found.c853eda5.js" rel="prefetch">
+    <link href="/js/not-found.84026c66.js" rel="prefetch">
     <link href="/js/register.5a1e5c40.js" rel="prefetch">
-    <link href="/js/reset-password.81abf779.js" rel="prefetch">
-    <link href="/js/user.07282674.js" rel="prefetch">
-    <link href="/js/users.11a65f94.js" rel="prefetch">
-    <link href="/css/app.e0014b86.css" rel="preload" as="style">
-    <link href="/js/app.dd444061.js" rel="preload" as="script">
+    <link href="/js/reset-password.dc0551d7.js" rel="prefetch">
+    <link href="/js/user.0855bf44.js" rel="prefetch">
+    <link href="/js/users.702d9728.js" rel="prefetch">
+    <link href="/css/app.568419d2.css" rel="preload" as="style">
+    <link href="/js/app.6321cd23.js" rel="preload" as="script">
     <link href="/js/chunk-vendors.334e7bfe.js" rel="preload" as="script">
-    <link href="/css/app.e0014b86.css" rel="stylesheet">
+    <link href="/css/app.568419d2.css" rel="stylesheet">
 </head>
 
 <body><noscript><strong>We're sorry but laravel-vue doesn't work properly without JavaScript enabled. Please enable it
             to continue.</strong></noscript>
     <div id="app"></div>
     <script src="/js/chunk-vendors.334e7bfe.js"></script>
-    <script src="/js/app.dd444061.js"></script>
+    <script src="/js/app.6321cd23.js"></script>
 </body>
 
 </html>
